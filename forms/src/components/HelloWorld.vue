53fbc0bbafc0243ef9fa1ca1effb198900ab73f9
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-25 15:15:16 +0530

    vuejs form handling

---
+++ forms/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+   <forms>
+    <div> 
+      <label for="name">Name : </label>
+      <input type="text" id="name">
+    </div>
+   </forms>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      formValues: {
+        name: ''
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
